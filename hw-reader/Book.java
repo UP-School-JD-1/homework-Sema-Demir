4fe356d2c91ade9b4517413cfc5c253238007189
semaderim <107874793+semaderim@users.noreply.github.com>
2022-08-18 13:01:30 +0300

    Add files via upload

---
+++ hw-reader/Book.java
@@ -0,0 +1,23 @@
+package hw;
+
+public class Book {
+
+	BookType bookType;
+	String name;
+	String title;
+	int page;
+	String type;
+	boolean isHardCover;
+	int currentPage;
+	Author author;
+	public void getbookType() {
+		
+		
+	}
+	
+		
+	}
+	
+	
+	
+
